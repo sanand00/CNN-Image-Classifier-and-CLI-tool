874053a99a93d7ddb309b6076636fa3c16fc016b
Siddharth Anand <sidaus1@gmail.com>
2024-02-15 21:20:31 +1100

    Refactored code to include 'if __name__ == "__main__":' consctruct. Minor change to use batch_size variable in test_loader function.

--- resnet_validate.py
+++ resnet_validate.py
@@ -13,33 +13,37 @@ import pandas as pd
 from datasets.dataset import Dataset
 from engines.validation import binary_validate
 
-with open('data/partition', 'rb') as file1, open('data/labels', 'rb') as file2:
-        partition = pickle.load(file1)
-        labels = pickle.load(file2)
+def main():
+    with open('data/partition', 'rb') as file1, open('data/labels', 'rb') as file2:
+            partition = pickle.load(file1)
+            labels = pickle.load(file2)
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-merge = Merge(inputdir = 'trained_models/res_net_18',
-              outputdir = 'trained_models/res_net_18',
-              outputfilename = 'res_net_18.pt')
-merge.merge()
-net = torch.load('trained_models/res_net_18/res_net_18.pt')
-os.remove('trained_models/res_net_18/res_net_18.pt')
-net.eval()
+    merge = Merge(inputdir = 'trained_models/res_net_18',
+                outputdir = 'trained_models/res_net_18',
+                outputfilename = 'res_net_18.pt')
+    merge.merge()
+    net = torch.load('trained_models/res_net_18/res_net_18.pt')
+    os.remove('trained_models/res_net_18/res_net_18.pt')
+    net.eval()
 
-batch_size = 32
+    batch_size = 32
 
-test_set = Dataset(partition['test'], labels)
-test_loader = torch.utils.data.DataLoader(test_set, batch_size = 32, shuffle = True)
+    test_set = Dataset(partition['test'], labels)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size, shuffle = True)
 
-validation_results = binary_validate(net, test_loader, device)
+    validation_results = binary_validate(net, test_loader, device)
 
-disp_cm = metrics.ConfusionMatrixDisplay(validation_results['confusion_matrix'])
-disp_cm.plot()
-disp_cm.figure_.savefig('trained_models/res_net_18/res_net_18_confusion_matrix.png')
+    disp_cm = metrics.ConfusionMatrixDisplay(validation_results['confusion_matrix'])
+    disp_cm.plot()
+    disp_cm.figure_.savefig('trained_models/res_net_18/res_net_18_confusion_matrix.png')
 
-disp_roc = validation_results['roc_curve']
-disp_roc.plot()
-disp_roc.figure_.savefig('trained_models/res_net_18/res_net_18_roc_curve.png')
+    disp_roc = validation_results['roc_curve']
+    disp_roc.plot()
+    disp_roc.figure_.savefig('trained_models/res_net_18/res_net_18_roc_curve.png')
 
-pd.DataFrame(validation_results['report']).transpose().to_csv('trained_models/res_net_18/res_net_18_classification_report.csv')
+    pd.DataFrame(validation_results['report']).transpose().to_csv('trained_models/res_net_18/res_net_18_classification_report.csv')
+    
+if __name__ == "__main__":
+    main()
